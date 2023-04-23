465d5b807667c8b44a19043f39533fcaab6616fb
shanxiee <shanxiee@qq.com>
2023-04-24 01:31:42 +0800

    NodeGraphProcessor可视化接入Odin,可视化修改

--- Assets/Plugins/com.alelievr.NodeGraphProcessor/Editor/NodeInspectorObject.cs
+++ Assets/Plugins/com.alelievr.NodeGraphProcessor/Editor/NodeInspectorObject.cs
@@ -4,94 +4,114 @@ using UnityEditor;
 using System;
 using System.Collections.Generic;
 using Object = UnityEngine.Object;
+using Sirenix;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Serialization;
 
 namespace GraphProcessor
 {
     /// <summary>
     /// Custom editor of the node inspector, you can inherit from this class to customize your node inspector.
     /// </summary>
-    [CustomEditor(typeof(NodeInspectorObject))]
-    public class NodeInspectorObjectEditor : Editor
-    {
-        NodeInspectorObject inspector;
-        protected VisualElement root;
-        protected VisualElement selectedNodeList;
-        protected VisualElement placeholder;
+    // [CustomEditor(typeof(NodeInspectorObject))]
+    // public class NodeInspectorObjectEditor : OdinEditor
+    // {
+        // [OdinSerialize]
+        // [Sirenix.OdinInspector.ShowInInspector]
+        // NodeInspectorObject inspector;
+        // protected VisualElement root;
+        // protected VisualElement selectedNodeList;
+        // protected VisualElement placeholder;
 
-        protected virtual void OnEnable()
-        {
-            inspector = target as NodeInspectorObject;
-            inspector.nodeSelectionUpdated += UpdateNodeInspectorList;
-            root = new VisualElement();
-            selectedNodeList = new VisualElement();
-            selectedNodeList.styleSheets.Add(Resources.Load<StyleSheet>("GraphProcessorStyles/InspectorView"));
-            root.Add(selectedNodeList);
-            placeholder = new Label("Select a node to show it's settings in the inspector");
-            placeholder.AddToClassList("PlaceHolder");
-            UpdateNodeInspectorList();
-        }
+        // protected virtual void OnEnable()
+        // {
+        //     inspector = target as NodeInspectorObject;
+        //     inspector.nodeSelectionUpdated += UpdateNodeInspectorList;
+            // root = new VisualElement();
+            // selectedNodeList = new VisualElement();
+            // selectedNodeList.styleSheets.Add(Resources.Load<StyleSheet>("GraphProcessorStyles/InspectorView"));
+            // root.Add(selectedNodeList);
+            // placeholder = new Label("Select a node to show it's settings in the inspector");
+            // placeholder.AddToClassList("PlaceHolder");
+            // UpdateNodeInspectorList();
+        // }
 
-        protected virtual void OnDisable()
-        {
-            inspector.nodeSelectionUpdated -= UpdateNodeInspectorList;
-        }
+        // protected virtual void OnDisable()
+        // {
+        //     inspector.nodeSelectionUpdated -= UpdateNodeInspectorList;
+        // }
 
-        public override VisualElement CreateInspectorGUI() => root;
+        // public override VisualElement CreateInspectorGUI() => root;
 
-        protected virtual void UpdateNodeInspectorList()
-        {
-            selectedNodeList.Clear();
+        // protected virtual void UpdateNodeInspectorList()
+        // {
+            // selectedNodeList.Clear();
 
-            if (inspector.selectedNodes.Count == 0)
-                selectedNodeList.Add(placeholder);
+            // if (inspector.selectedNodes.Count == 0)
+            //     selectedNodeList.Add(placeholder);
 
-            foreach (var nodeView in inspector.selectedNodes)
-                selectedNodeList.Add(CreateNodeBlock(nodeView));
-        }
+            //实际上的序列化显示
+            //CreateNodeBlock中把选中的节点计入进VisualElement中
+            // foreach (var nodeView in inspector.selectedNodes)
+            //     selectedNodeList.Add(CreateNodeBlock(nodeView));
+        // }
 
-        protected VisualElement CreateNodeBlock(BaseNodeView nodeView)
-        {
-            var view = new VisualElement();
-
-            view.Add(new Label(nodeView.nodeTarget.name));
-
-            var tmp = nodeView.controlsContainer;
-            nodeView.controlsContainer = view;
-            nodeView.Enable(true);
-            nodeView.controlsContainer.AddToClassList("NodeControls");
-            var block = nodeView.controlsContainer;
-            nodeView.controlsContainer = tmp;
-            
-            return block;
-        }
-    }
+        // protected VisualElement CreateNodeBlock(BaseNodeView nodeView)
+        // {
+        //     var view = new VisualElement();
+        //
+        //     view.Add(new Label(nodeView.nodeTarget.name));
+        //
+        //     var tmp = nodeView.controlsContainer;
+        //     nodeView.controlsContainer = view;
+        //     nodeView.Enable(true);
+        //     nodeView.controlsContainer.AddToClassList("NodeControls");
+        //     var block = nodeView.controlsContainer;
+        //     nodeView.controlsContainer = tmp;
+        //
+        //     return block;
+        // }
+    // }
 
     /// <summary>
     /// Node inspector object, you can inherit from this class to customize your node inspector.
     /// </summary>
-    public class NodeInspectorObject : ScriptableObject
+    public class NodeInspectorObject : SerializedScriptableObject
     {
-        /// <summary>Previously selected object by the inspector</summary>
-        public Object previouslySelectedObject;
+
+        
+        // [Sirenix.OdinInspector.ShowInInspector]
         /// <summary>List of currently selected nodes</summary>
-        public HashSet<BaseNodeView> selectedNodes { get; private set; } = new HashSet<BaseNodeView>();
-
-        /// <summary>Triggered when the selection is updated</summary>
-        public event Action nodeSelectionUpdated;
-
-        /// <summary>Updates the selection from the graph</summary>
-        public virtual void UpdateSelectedNodes(HashSet<BaseNodeView> views)
-        {
-            selectedNodes = views;
-            nodeSelectionUpdated?.Invoke();
-        }
-
-        public virtual void RefreshNodes() => nodeSelectionUpdated?.Invoke();
-
+        public HashSet<BaseNodeView> selectedNodes = new HashSet<BaseNodeView>();
+        
+        // /// <summary>Updates the selection from the graph</summary>
+        // public virtual void UpdateSelectedNodes(HashSet<BaseNodeView> views)
+        // {
+        //     selectedNodes = views;
+        //
+        // }
+        
         public virtual void NodeViewRemoved(BaseNodeView view)
         {
             selectedNodes.Remove(view);
-            nodeSelectionUpdated?.Invoke();
+    
+        }
+    }
+    
+    /// <summary>
+    /// 手动清理NodeInspectorObject，防止出现编辑已经非法的数据，因为NodeView是每次重新编译/PlayMode后重新构建的
+    /// 所以如果这里不做清理在重新编译/PlayMode后编辑的就是已经失效的数据
+    /// </summary>
+    public class ResetSelectNodeInfo : Editor
+    {
+        [InitializeOnLoadMethod]
+        public static void _ResetSelectNodeInfo()
+        {
+            if (Selection.activeObject is NodeInspectorObject nodeInspectorObject)
+            {
+                nodeInspectorObject.selectedNodes.Clear();
+            }
         }
     }
 }
